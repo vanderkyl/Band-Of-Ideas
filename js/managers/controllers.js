03819f7260067944d1cd2862411f99788aaecd16
Kyle Vanderhoof <vandermail@utexas.edu>
2022-04-01 05:19:27 -0500

    another fix

--- js/managers/controllers.js
+++ js/managers/controllers.js
@@ -59,29 +59,3 @@ function updateFileViews(http, file) {
             console.log(response.data);
           });
 }
-
-function addToSong(id) {
-  var data = new FormData();
-  data.append('songId', CURRENT_SONG.id);
-  data.append('fileId', id);
-  data.append('bandId', CURRENT_BAND.id);
-  data.append('userId', CURRENT_USER.id);
-
-    if (window.XMLHttpRequest) {
-      // code for IE7+, Firefox, Chrome, Opera, Safari
-      xmlhttp=new XMLHttpRequest();
-    } else {  // code for IE6, IE5
-      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange=function() {
-      if (this.readyState==4 && this.status==200) {
-        console.log("Success");
-        console.log(this.ResponseText);
-        hideElementById("ideaAddButton-" + id);
-        displayElementById("ideaAdded-" + id);
-      }
-    }
-    xmlhttp.open("POST","/php/addToSong.php", true);
-    xmlhttp.send(data);
-  }
-}
